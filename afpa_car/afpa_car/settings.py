fa7a2fec9a2cf6f86d48df509d96b111e9f4501f
Demky <demky.box@gmail.com>
2018-07-12 16:23:48 +0200

    merged Front and Users branch to Devellop

--- afpa_car/afpa_car/settings.py
+++ afpa_car/afpa_car/settings.py
@@ -12,7 +12,7 @@ https://docs.djangoproject.com/en/2.0/ref/settings/
 
 import os
 
-from .databases_settings import DATABASES 
+from .databases_settings import DATABASES
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
