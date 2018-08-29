a0a0b68b82556a3912ca767b3d465a7cc2dd37ae
Demky <37147299+Demky@users.noreply.github.com>
2018-08-29 08:40:12 +0200

    Update databases_settings.py

--- afpa_car/afpa_car/databases_settings.py
+++ afpa_car/afpa_car/databases_settings.py
@@ -1,20 +1,26 @@
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'afpa_car3',
-        'USER': 'pc_Boris',
-        'PASSWORD': '',
-        'HOST': '10.111.62.2',
-        'PORT': '',
-        'OPTIONS': {
-        "init_command": "SET foreign_key_checks = 0;",
-        },
-    }
-}
-
+# MARIADB
 # DATABASES = {
 #     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': 'afpa_car_maison',
+#         'ENGINE': 'django.db.backends.mysql',
+#         'NAME': 'afpa_car2',
+#         'USER': 'pc_Boris',
+#         'PASSWORD': '',
+#         'HOST': '10.111.62.2',
+#         'PORT': '',
+#         'OPTIONS': {
+#         "init_command": "SET foreign_key_checks = 0;",
+#         },
 #     }
 # }
+
+
+#SQLITE3
+import os
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3')
+    }
+}
